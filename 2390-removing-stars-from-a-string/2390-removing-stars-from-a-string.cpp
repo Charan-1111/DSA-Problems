5cebf100f4ed8ae037b355636daf92a33bd5375a
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-07-01 12:27:17 +0530

    Time: 16 ms (68.91%), Space: 28.4 MB (75.6%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string res;
+
+        for(char ch : s) {
+            if(ch != '*') {
+                res.push_back(ch);
+            } else {
+                res.pop_back();
+            }
+        }
+
+        return res;
+    }
+};
