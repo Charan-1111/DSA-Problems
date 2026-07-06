8d1fc4982cd9c6d2b285b5bd416cb5e5b9b5ecdc
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-07-06 12:58:33 +0530

    Time: 0 ms (100%), Space: 9 MB (37.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> mp;
+
+        mp[')'] = '(';
+        mp['}'] = '{';
+        mp[']'] = '[';
+
+        stack<char> st;
+
+        for(char ch : s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if(st.empty() || st.top() != mp[ch]) {
+                    return false;
+                }  else {
+                    st.pop();
+                }
+            }
+        }
+
+        return st.empty();        
+    }
+};
