47f7b3ed674bcf024f9575655f936b62f365514f
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2026-09-03 07:46:45 +0530

    Time: 0 ms (100%), Space: 17.4 MB (89%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,61 @@
 class Solution {
 public:
-    int findLowerBound(vector<int> &nums, int target) {
-        int res = -1;
-
-        int l = 0, r = nums.size()-1;
-
-        while(l <= r) {
-            int mid = l + ((r-l) >> 1);
-
-            if(nums[mid] == target) {
-                res = mid;
-                r = mid - 1;
-            } else if(nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+   int first_occurance(vector<int>&nums, int t){
+        int l=0;
+        int r = nums.size()-1;
+        int ans = -1;
+        
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            
+            if(nums[mid] == t){
+                ans = mid;
+                r = mid-1;
             }
+            
+            else if(nums[mid] < t)
+                l = mid+1;
+            
+            else
+                r = mid-1;
+        
         }
-
-        return res;
+        
+        return ans;
     }
-
-    int findUpperBound(vector<int> &nums, int target) {
-        int res = -1;
-        int l = 0, r = nums.size()-1;
-
-        while(l <= r) {
-            int mid = l + ((r-l) >> 1);
-
-            if(nums[mid] == target) {
-                res = mid;
-                l = mid + 1;
-            } else if(nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+    
+    int last_occurance(vector<int>&nums, int t){
+        int l=0;
+        int r = nums.size()-1;
+        int ans = -1;
+        
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            
+            if(nums[mid] == t){
+                ans = mid;
+                l = mid+1;
             }
+            
+            else if(nums[mid] < t)
+                l = mid+1;
+            
+            else
+                r = mid-1;
+        
         }
-
-        return res;
+        
+        return ans;
     }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = findLowerBound(nums, target);
-        int second = findUpperBound(nums, target);
-
-        return {first, second};
+        vector<int> ans;
+        
+        int fst = first_occurance(nums, target);
+        int lst = last_occurance(nums, target);
+        
+        ans.push_back(fst);
+        ans.push_back(lst);
+        
+        return ans;
     }
 };
